7998fe306aa152e1afe4b204521715822950a6c8
Unknown <lasia.lo@duke.edu>
2018-03-25 23:41:38 -0400

    Adding Folder Dialogue

--- src/Parser.py
+++ src/Parser.py
@@ -1,2 +1,25 @@
-class FileParser(file):
-    
+import os
+import glob
+
+"""
+@author Lasia Lo
+"""
+
+
+class FolderParser:
+    """Parses through a folder
+
+       Attributes:
+            folder : Name of folder to parse
+    """
+    def __init__(self,foldername):
+        self.folder = glob.glob(foldername)
+
+    def parse(self):
+
+        print(os.getcwd())
+        # for f in myfolder
+        #     file = open(f, 'r')
+
+
+print("asdsadsad")
--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,15 @@
 import Parser
+import tkinter as tk
+from tkinter import filedialog
 
-parser = Parser.parse()
+def main():
+    parser = Parser.FileParser()
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askdirectory()
+    parse = Parser.FolderParser(file_path)
+    print(file_path)
+
+
+if __name__ == "__main__":
+    main()
