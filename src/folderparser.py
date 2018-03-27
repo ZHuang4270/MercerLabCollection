3582de4db04c0c459aa7a3e0d976c37900d90467
Unknown <lasia.lo@duke.edu>
2018-03-27 13:14:52 -0400

    Added  product method

--- src/folderparser.py
+++ src/folderparser.py
@@ -2,6 +2,8 @@ import glob
 import os
 
 
+
+
 class FolderParser:
     # """Parses through a folder
     #
@@ -20,17 +22,17 @@ class FolderParser:
 
     def parse(self):
         for f in self.read:
-            self.create_folder(f)
+            folder = self.create_folder(f)
 
             file = open(f, 'r')
+            writefile = 'NULL'
+            for line in file:
+                if line[0] == '>':
+                    writefile = self.product(line)
+                if writefile != 'NULL':
+                    self.write_to(writefile)
 
-            # for line in file:
-            #     line = line.split()
-            #     if line.__contains__()
-            #     if self.is_header(line):
-            #         print(line)
-            #         print(line.split())
-            #         print('------------')
+
 
     def create_folder(self, filename):
         filename = os.path.basename(filename)
@@ -39,9 +41,16 @@ class FolderParser:
         if not os.path.exists(directory):
             os.makedirs(directory)
             # for s in self.products:
+                #make a file for each s
+        return
 
-    def is_header(self, line):
-        return line[0] == '>'
+    def product(self, line):
+        to_return = 'NULL'
+        for p in self.products:
+            key = 'product=' + p + 'S'
+            if line.find(key) != -1:
+                to_return = p
+        return to_return
 
-def why():
-    print("why")
+    def write_to(self,writefile):
+        pass
